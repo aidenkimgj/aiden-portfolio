0559eadbaf6949946fbb59b02caa346da5407617
Aiden Kim <aidengookjinkim@gmail.com>
2021-05-11 21:37:55 -0600

    Create Navbar

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Container,
+  Form,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from 'reactstrap';
+const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-const navigation = () => {
+  const handleToggle = () => setIsOpen(!isOpen);
+
   return (
     <>
-      <nav id="nav">hi</nav>
+      <Navbar color="dark" dark expand="lg" className="sticky-top">
+        <Container>
+          <NavbarBrand href="/">Aiden</NavbarBrand>
+          <NavbarToggler onClick={handleToggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav
+              className="ml-auto d-felx justify-content-around"
+              navbar
+              style={{ fontWeight: 'bold' }}
+            >
+              <NavItem>
+                <Form className="col pr-3 p-1">
+                  <NavLink href="#about" className="text-decoration-none">
+                    About
+                  </NavLink>
+                </Form>
+              </NavItem>
+              <NavItem>
+                <Form className="col pr-3 p-1">
+                  <NavLink href="#skills" className="text-decoration-none">
+                    Skills
+                  </NavLink>
+                </Form>
+              </NavItem>
+              <NavItem>
+                <Form className="col pr-3 p-1">
+                  <NavLink href="#work" className="text-decoration-none">
+                    Work
+                  </NavLink>
+                </Form>
+              </NavItem>
+              <NavItem>
+                <Form className="col pr-3 p-1">
+                  <NavLink
+                    href="#testimonials"
+                    className="text-decoration-none"
+                  >
+                    Testimonials
+                  </NavLink>
+                </Form>
+              </NavItem>
+              <NavItem>
+                <Form className="col pr-3 p-1">
+                  <NavLink href="#contact" className="text-decoration-none">
+                    Contact
+                  </NavLink>
+                </Form>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
 
-export default navigation;
+export default Navigation;
