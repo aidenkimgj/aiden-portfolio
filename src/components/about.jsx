e9f3e6fc20de3de322bd00d08a93c7aa8f2f3b75
Aiden Kim <aidengookjinkim@gmail.com>
2021-05-27 23:42:20 -0600

    First styling About component

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -14,23 +14,28 @@ const About = () => {
     <>
       <div id="about" className="section section__container">
         <h1>About me</h1>
-        <p>
-          Hello, my future employers! I'm <b>Aiden Kim</b>, dreaming of becoming
-          a full-stack developer who believes my code can change someone's world
-          and tries to write as much code as possible. <br />
-          It is safe to say that the rapid growth of many industries in modern
-          society has been driven by advances in programming based on
-          information technology. In an era of growing programmer's role in
-          helping people lead a better life through continuous problem solving,
-          I am also trying hard to become a part of that role. <br />I look
-          forward to the opportunity to demonstrate my skills and contribute to
-          the success of my future employer by contributing significantly to all
-          projects given to me.
-        </p>
+        <div className="about__me">
+          <p>
+            Hello, my future employers! I'm <b>Aiden Kim</b>, dreaming of
+            becoming a full-stack developer who believes my code can change
+            someone's world and tries to write as much code as possible. <br />
+            It is safe to say that the rapid growth of many industries in modern
+            society has been driven by advances in programming based on
+            information technology. In an era of growing programmer's role in
+            helping people lead a better life through continuous problem
+            solving, I am also trying hard to become a part of that role. <br />
+            I look forward to the opportunity to demonstrate my skills and
+            contribute to the success of my future employer by contributing
+            significantly to all projects given to me.
+          </p>
+        </div>
+
         <div className="about__majors">
           <div className="major">
             <div className="major__icon">
-              <FontAwesomeIcon color="grey" icon={faLaptopCode} size="3x" />
+              <div>
+                <FontAwesomeIcon icon={faLaptopCode} />
+              </div>
             </div>
             <div className="major__title">Front-end</div>
             <div className="major__description">
@@ -42,7 +47,9 @@ const About = () => {
 
           <div className="major">
             <div className="major__icon">
-              <FontAwesomeIcon color="grey" icon={faMobile} size="3x" />
+              <div>
+                <FontAwesomeIcon icon={faMobile} />
+              </div>
             </div>
             <div className="major__title">Mobile</div>
             <div className="major__description">React Native</div>
@@ -50,7 +57,9 @@ const About = () => {
 
           <div className="major">
             <div className="major__icon">
-              <FontAwesomeIcon color="grey" icon={faServer} size="3x" />
+              <div>
+                <FontAwesomeIcon icon={faServer} />
+              </div>
             </div>
             <div className="major__title">Back-end</div>
             <div className="major__description">
@@ -71,7 +80,7 @@ const About = () => {
             />
             <div className="job__description">
               <p className="job__name">
-                Southern Alberta Institute of Technology <br /> Software
+                <b>Southern Alberta Institute of Technology</b> <br /> Software
                 Development
               </p>
               <p className="job__period">2020 Jan - Until now</p>
@@ -79,18 +88,20 @@ const About = () => {
           </div>
 
           <div className="job">
-            <Image
-              src={DonggukLogo}
-              alt="Dongguk"
-              className="job__logo"
-              id="job__logo"
-            />
-            <div className="job__description">
-              <p className="job__name">
-                Dongguk University <br />
-                Economics, Political Diplomacy
-              </p>
-              <p className="job__period">2005 March - 2011 August</p>
+            <div className="job2">
+              <Image
+                src={DonggukLogo}
+                alt="Dongguk"
+                className="job__logo"
+                id="job__logo2"
+              />
+              <div className="job__description">
+                <p className="job__name">
+                  <b>Dongguk University</b> <br />
+                  Economics, Political Diplomacy
+                </p>
+                <p className="job__period">2005 March - 2011 August</p>
+              </div>
             </div>
           </div>
         </div>
